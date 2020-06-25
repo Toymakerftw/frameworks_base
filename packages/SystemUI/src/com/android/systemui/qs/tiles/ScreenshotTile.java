ArrowOS/android_frameworks_base

SystemUI: Add QS screenshot tile
Change-Id: Iac2215a98d50873905c208ea27f491ec62810bfc

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2020 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.view.WindowManager;
+import static android.view.WindowManager.TAKE_SCREENSHOT_FULLSCREEN;
+import static android.view.WindowManager.TAKE_SCREENSHOT_SELECTED_REGION;
+
+import com.android.internal.util.ScreenshotHelper;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.internal.R;
+
+import javax.inject.Inject;
+
+/** Quick settings tile: Screenshot **/
+public class ScreenshotTile extends QSTileImpl<BooleanState> {
+
+    @Inject
+    public ScreenshotTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.ARROW;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {}
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void handleClick() {
+        mHost.collapsePanels();
+        final ScreenshotHelper screenshotHelper = new ScreenshotHelper(mContext);
+        mHandler.postDelayed(() -> {
+            screenshotHelper.takeScreenshot(TAKE_SCREENSHOT_FULLSCREEN, true, true, mHandler, null);
+        }, 1000);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public void handleLongClick() {
+        mHost.collapsePanels();
+        final ScreenshotHelper screenshotHelper = new ScreenshotHelper(mContext);
+        mHandler.postDelayed(() -> {
+            screenshotHelper.takeScreenshot(TAKE_SCREENSHOT_SELECTED_REGION, true, true, mHandler, null);
+        }, 1000);
+        refreshState();
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.global_action_screenshot);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.label = mContext.getString(
+                R.string.global_action_screenshot);
+        state.contentDescription =  mContext.getString(
+                R.string.global_action_screenshot);
+        state.icon = ResourceIcon.get(R.drawable.ic_screenshot);
+        state.value = true;
+        state.state = Tile.STATE_ACTIVE;
+    }
+}

Toymakerftw/frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2020 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.view.WindowManager;
+import static android.view.WindowManager.TAKE_SCREENSHOT_FULLSCREEN;
+import static android.view.WindowManager.TAKE_SCREENSHOT_SELECTED_REGION;
+
+import com.android.internal.util.ScreenshotHelper;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.internal.R;
+
+import javax.inject.Inject;
+
+/** Quick settings tile: Screenshot **/
+public class ScreenshotTile extends QSTileImpl<BooleanState> {
+
+    @Inject
+    public ScreenshotTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.BOOTLEG;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {}
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void handleClick() {
+        mHost.collapsePanels();
+        final ScreenshotHelper screenshotHelper = new ScreenshotHelper(mContext);
+        mHandler.postDelayed(() -> {
+            screenshotHelper.takeScreenshot(TAKE_SCREENSHOT_FULLSCREEN, true, true, mHandler, null);
+        }, 1000);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public void handleLongClick() {
+        mHost.collapsePanels();
+        final ScreenshotHelper screenshotHelper = new ScreenshotHelper(mContext);
+        mHandler.postDelayed(() -> {
+            screenshotHelper.takeScreenshot(TAKE_SCREENSHOT_SELECTED_REGION, true, true, mHandler, null);
+        }, 1000);
+        refreshState();
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.global_action_screenshot);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.label = mContext.getString(
+                R.string.global_action_screenshot);
+        state.contentDescription =  mContext.getString(
+                R.string.global_action_screenshot);
+        state.icon = ResourceIcon.get(R.drawable.ic_screenshot);
+        state.value = true;
+        state.state = Tile.STATE_ACTIVE;
+    }
+}